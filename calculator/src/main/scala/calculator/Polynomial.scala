89d3f7836dc7392281e5f38903783f7c13b349a8
aar <aar@lesfurets.com>
2015-04-23 08:56:16 +0200

    Week2: Polynomial solver implementation

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,19 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal( b()*b()-4*a()*c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal{
+      val d = delta()
+      val aa = a()
+      val bb = b()
+      val cc = c()
+      if (d==0) {Set(-bb/2*aa)}
+      else if(d>0) Set(-bb-Math.sqrt(d)/2*aa,-bb+Math.sqrt(d)/2*aa)
+      else if(d==d)Set.empty else Set.empty
+    }
   }
 }
