03d2fef3932fd57bb9e671ffe9a470fb8d5ebf41
Andrei ARION <andrei.arion@gmail.com>
2015-04-28 09:03:21 +0200

    Week2 : Calculator cycle detection

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -1,42 +1,48 @@
 package calculator
 
 sealed abstract class Expr
+
 final case class Literal(v: Double) extends Expr
+
 final case class Ref(name: String) extends Expr
+
 final case class Plus(a: Expr, b: Expr) extends Expr
+
 final case class Minus(a: Expr, b: Expr) extends Expr
+
 final case class Times(a: Expr, b: Expr) extends Expr
+
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
   def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    var res = Map[String,Signal[Double]]()
-    for(ne <- namedExpressions){
+                     namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    var res = Map[String, Signal[Double]]()
+    for (ne <- namedExpressions) {
       val name = ne._1
       val expr = ne._2
-      res += ne._1->Signal(eval(expr(),namedExpressions))
+      res += ne._1 -> Signal(eval(expr(), namedExpressions.filter(el => el._1 != name)))
     }
     res
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    expr match  {
+    expr match {
       case Literal(v) => v
-      case Ref(n) => eval(getReferenceExpr(n,references),references)
-      case Plus(a,b)=>eval(a,references)+eval(b,references)
-      case Minus(a,b)=>eval(a,references)-eval(a,references)
-      case Times(a,b)=>eval(a,references)*eval(a,references)
-      case Divide(a,b)=>eval(a,references)/eval(a,references)
+      case Ref(n) => eval(getReferenceExpr(n, references), references)
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(a, references)
+      case Times(a, b) => eval(a, references) * eval(a, references)
+      case Divide(a, b) => eval(a, references) / eval(a, references)
     }
-    
+
   }
 
   /** Get the Expr for a referenced variables.
-   *  If the variable is not known, returns a literal NaN.
-   */
+    * If the variable is not known, returns a literal NaN.
+    */
   private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
+                               references: Map[String, Signal[Expr]]) = {
     references.get(name).fold[Expr] {
       Literal(Double.NaN)
     } { exprSignal =>
